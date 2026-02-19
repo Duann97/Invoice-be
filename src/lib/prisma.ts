9bfe12c918a08bf730ef1d7c65c45e04fa1016e0
Duann97 <duanbunar97@gmail.com>
2026-02-19 13:05:03 +0700

    fix: fffinal invoice be

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString }, { schema: "invoice-app" });
 
 export const prisma = new PrismaClient({ adapter }) as unknown as PrismaClient;
